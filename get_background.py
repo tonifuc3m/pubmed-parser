dbe4916d5969c8f15978fc1e8acdc02cd084bc81
antonio miranda <antonio.miranda@alumnos.uc3m.es>
2021-08-19 10:31:49 +0200

    Save PMC and MeSH terms in TSV

--- get_background.py
+++ get_background.py
@@ -13,6 +13,7 @@ import datetime
 import re
 import argparse
 import pickle
+import json
 
 _RE_COMBINE_WHITESPACE = re.compile(r"\s+")
 
@@ -22,7 +23,7 @@ def search(query):
     Entrez.email = 'PLACEHOLDER@gmail.com'
     handle = Entrez.esearch(db='pubmed',
                             sort='relevance',
-                            retmax='2000000',
+                            retmax='20000000',
                             retmode='xml',
                             term=query) 
     results = Entrez.read(handle)
@@ -43,12 +44,15 @@ def chunks(lst, n):
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
         
-def get_PMIDs(queries, fnames, base_path, logfile, all_time=False):
+def get_PMIDs(queries, fnames, base_path, logfile, all_time=False, has_abstract=True):
     sets = []
     for query,fname in zip(queries, fnames):
-        complete_query = f'{query} AND (hasabstract)'
+        complete_query = query
+        if has_abstract==True:
+             complete_query = complete_query + ' AND (hasabstract)'
         if all_time==False:
-             complete_query = complete_query + 'AND (2011/01/01:2021/12/31[dp])'
+             complete_query = complete_query + ' AND (2011/01/01:2021/12/31[dp])'
+
         r = search(complete_query)
         logfile.write(fname + '\t' + str(len(set(r['IdList']))) + '\n')
         fout = open(os.path.join(base_path, f'pmids/{fname}.txt'), 'w')
@@ -76,8 +80,18 @@ def basic_text_cleaning(s, _RE_COMBINE_WHITESPACE):
     s_final = _RE_COMBINE_WHITESPACE.sub(" ", s).strip()
     return s_final
 
-def parse_abstract(abstract, PMID, logfile):
+def parse_abstract(result, logfile, PMID):
     
+    # Skip records with no abstract
+    if 'Abstract' not in result['MedlineCitation']['Article'].keys():
+        logfile.write(f'WARNING: Skipping {PMID}: Abstract key not in record\n')
+        return ''
+    if 'AbstractText' not in result['MedlineCitation']['Article']['Abstract'].keys():
+        logfile.write(f'WARNING: Skipping {PMID}: AbstractText key not in record\n')
+        return ''
+    
+    abstract = result['MedlineCitation']['Article']['Abstract']['AbstractText']
+            
     # Parse structured abstracts
     if len(abstract)>1:
         abstract_str = ''
@@ -108,10 +122,45 @@ def parse_abstract(abstract, PMID, logfile):
                 
     return abstract_str
     
+def get_PMC(article):
+    if 'ArticleIdList' not in article['PubmedData'].keys():
+        return ''
+    for item in article['PubmedData']['ArticleIdList']:
+        if 'PMC' in item:
+            return item.strip('PMC')
+    return ''
+        
+def get_mesh_dict(result, logfile, PMID):
+    # Parse Qualifiers and Descriptors
+    if 'MeshHeadingList' not in result['MedlineCitation']:
+        logfile.write(f"{PMID} does not has MeshHeadingList key in XML, unable to parse it\n")
+        return set(),set()
+    
+    qualifiers = []
+    descriptors = []    
+    
+    for msh in result['MedlineCitation']['MeshHeadingList']:
+        # Parse Qualifiers
+        if type(msh['QualifierName']) != list:
+            logfile.write(f"{PMID} has Qualifiers not properly saved, unable to parse it\n")
+            return set(),set()
+        qs = msh['QualifierName']
+        if len(qs)>0:
+            for q in qs:
+                qualifiers.append(q.attributes['UI'])
+                
+        # Parse Descriptors
+        if type(msh['DescriptorName']) == list:
+            logfile.write(f"{PMID} has Descriptors not properly saved, unable to parse it\n")
+            return set(),set()
+        descriptors.append(msh['DescriptorName'].attributes['UI'])
+           
+    return set(qualifiers), set(descriptors)
+        
+            
 def get_full_records(base_path, setsv2, logfile):
     
     c = 0
-    # check = []
     logfile.write('Chunks:\n')
     for item in sorted(list(chunks(list(setsv2), 10000))):
         c=c+1
@@ -119,34 +168,31 @@ def get_full_records(base_path, setsv2, logfile):
         logfile.write(f'{c}\n')
         res = fetch_details(item)
         
+        # Write whole results 
+        with open(os.path.join(base_path, f'{c}.json'), 'w') as file:
+            file.write(json.dumps({c: res}))
+        
+        # Generate PMID, title, abstract file
         for result in res['PubmedArticle']:
+            
+            # Get PMID
             PMID = str(result['MedlineCitation']['PMID'])
-            #logfile.write(f'{PMID}\n') # Added
+            # Get PMC
+            PMC = get_PMC(result)
+            # Get title
             title = result['MedlineCitation']['Article']['ArticleTitle'].strip()
-            
-            # Skip records with no abstract
-            if 'Abstract' not in result['MedlineCitation']['Article'].keys():
-                logfile.write(f'WARNING: Skipping {PMID}: Abstract key not in record\n')
-                continue
-            if 'AbstractText' not in result['MedlineCitation']['Article']['Abstract'].keys():
-                logfile.write(f'WARNING: Skipping {PMID}: AbstractText key not in record\n')
-                continue
-            
-            abstract = result['MedlineCitation']['Article']['Abstract']['AbstractText']
-            
-            abstract_str = parse_abstract(abstract, PMID, logfile)
+            # Get MeSH terms
+            qualifiers, descriptors = get_mesh_dict(result, logfile, PMID)
+            # Get Abstract
+            abstract_str = parse_abstract(result, logfile, PMID)
                 
             # Basic text cleaning
             abstract_str = basic_text_cleaning(abstract_str, _RE_COMBINE_WHITESPACE)
             title = basic_text_cleaning(title, _RE_COMBINE_WHITESPACE)
             
-            """if any(x in abstract_str for x in ['\t', '\n']):
-                logfile.write(f'WARNING: Skipping {PMID}: Abstract contains invalid characters\n')
-                continue"""
+            fout.write(f"{PMID}\t{title}\t{abstract_str}\t{PMC}\t{','.join(sorted(qualifiers))}\t{','.join(sorted(descriptors))}\n")
             
-            fout.write(f'{PMID}\t{title}\t{abstract_str}\n')
         fout.close()
-    # return check
 
 
 def argparser():
@@ -191,10 +237,9 @@ if __name__ == '__main__':
         
     elif mode=='search_pmids':
         
-        '''
         base_path, rules_name, logfilepath = argparser()
         # rules_name = 'criteria10-rules.txt''
-        # base_path = '/home/antonio/Documents/Projects/Tasks/biocreative/background-set/my-downloaded-files/criteria10-all-time'
+        # base_path = '$HOME/tmp/criteria10-all-time'
         logfile = open(logfilepath, 'w')
         
         queries = list(map(lambda x: x.strip('\n'), open(os.path.join(base_path, rules_name)).readlines()))
@@ -227,5 +272,5 @@ if __name__ == '__main__':
         logfile.write(f'Store PMIDs in {os.path.join(base_path, "pmids")}\n')
         setsv2 = get_PMIDs(queries, fnames, base_path, logfile, all_time=True)
         logfile.write(f'{datetime.datetime.now()}: Total number of unique PMIDs: {str(len(setsv2))}\n')
-        logfile.close()'''
+        logfile.close()
 
